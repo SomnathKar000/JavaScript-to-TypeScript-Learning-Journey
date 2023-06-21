e565b4fd67d30e0fa0092851d35fdd31e312aa91
SomnathKar000 <somnathkar201901@gmail.com>
2023-06-21 16:48:04 +0530

    new changes

--- dist/build-in types.js
+++ dist/build-in types.js
@@ -3,7 +3,20 @@ let num = 1234565;
 let course = "Hello world";
 let is_published = false;
 let level;
+level = 54;
+level = "age";
 const printValues = (value) => {
     console.log(value);
 };
+const small = 1;
+const medium = 12;
+const large = 23;
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 0] = "Small";
+    Size[Size["Medium"] = 12] = "Medium";
+    Size[Size["Large"] = 20] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=build-in%20types.js.map
--- src/build-in types.ts
+++ src/build-in types.ts
@@ -3,7 +3,27 @@ let course: string = "Hello world";
 let is_published: boolean = false;
 
 let level; // It is an any veriable in TypeScript
+level = 54;
+level = "age";
 
 const printValues = (value: any) => {
   console.log(value);
 };
+
+// Enums
+
+const small = 1;
+const medium = 12;
+const large = 23;
+
+// PascalCase
+
+enum Size {
+  Small = 0,
+  Medium = 12,
+  Large = 20,
+}
+
+let mySize: Size = Size.Medium;
+
+console.log(mySize);
