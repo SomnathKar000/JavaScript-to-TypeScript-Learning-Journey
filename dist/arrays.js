516e1a3df4aff771ac9b7ac26ef9aa2196f6ffd0
SomnathKar000 <somnathkar201901@gmail.com>
2023-07-01 20:27:56 +0530

    removed dist folder

--- dist/arrays.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let numbers = [1, 2, 3, 5];
-let objArray = [{ start: 1 }, {}, {}];
-let user = [1, "Hello world"];
-//# sourceMappingURL=arrays.js.map
--- dist/optional_chaining.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a;
-const getCustomer = (id) => {
-    return id === 0 ? null : { birthDay: new Date() };
-};
-let customer = getCustomer(1);
-console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthDay) === null || _a === void 0 ? void 0 : _a.getFullYear());
-//# sourceMappingURL=optional_chaining.js.map
