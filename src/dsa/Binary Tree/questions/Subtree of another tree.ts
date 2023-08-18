b19081ff3536f5d2ab6d7276ad1e354336a1c9d6
SomnathKar000 <somnathkar201901@gmail.com>
2023-08-18 23:26:06 +0530

    new changes

---
+++ src/dsa/Binary Tree/questions/Subtree of another tree.ts
@@ -0,0 +1,28 @@
+const isIdentical = (
+  root: TreeNode | null,
+  subRoot: TreeNode | null
+): boolean => {
+  if (root === null && subRoot === null) {
+    return true;
+  }
+  if (root === null || subRoot === null) {
+    return false;
+  }
+  if (root.val !== subRoot.val) {
+    return false;
+  }
+  return (
+    isIdentical(root.left, subRoot.left) &&
+    isIdentical(root.right, subRoot.right)
+  );
+};
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+  if (root === null) {
+    return false;
+  }
+  if (isIdentical(root, subRoot)) {
+    return true;
+  }
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+}
